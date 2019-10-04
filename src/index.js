3b567bea62469c74f0e9859851c62c24e6ed8f52
Alivarik <45916819+Alivarik@users.noreply.github.com>
2019-10-05 01:36:13 +0300

    index.js: add solution

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+
+function getTimeForEducation(focus, knowsProgramming, speedConfig) {
+    if (knowsProgramming == true) {
+        return 800 / speedConfig[focus];
+    } else {
+        return 500 / speedConfig[focus];
+    }
+}
